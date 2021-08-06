31155399b75439c76619f6479070e60d1be85326
Bharat <bharatprajapati2503@gmail.com>
2021-08-06 18:38:53 +0530

    initial commit

---
+++ css/script.js
@@ -0,0 +1,10 @@
+function fun1(){
+    // document.getElementsByClassName('nav2').style.margineleft = '100%';
+    document.getElementById('nav2').style.marginLeft = '70%';
+    
+}
+function fun2 {
+    document.getElementById('nav2').style.marginLeft = '100%';
+
+    
+}
